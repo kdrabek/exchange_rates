796624be86dc625c5b8e8b9e3e78746191c23a4f
Krzysztof Drabek <kdrabek@gmail.com>
2016-11-11 18:12:58 +0100

    Basic models.

--- exchange_rates/rates/models.py
+++ exchange_rates/rates/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+
+    code = models.CharField(max_length=3, primary_key=True)
+    name = models.CharField(max_length=100)
+    table_type = models.CharField(max_length=1)
+
+
+class Rate(models.Model):
+
+    code = models.ForeignKey('Currency')
+    rate = models.DecimalField(max_digits=6, decimal_places=4)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    table_id = models.ForeignKey('Table')
+
+
+class Table(models.Model):
+
+    table_id = models.CharField(max_length=50, primary_key=True)
+    type = models.CharField(max_length=1)
+    effective_date = models.DateField(auto_now=False, auto_now_add=False)
+    when_fetched = models.DateTimeField(auto_now=False, auto_now_add=True)
--- run_web.sh
+++ run_web.sh
@@ -2,6 +2,9 @@
 
 sleep 10
 cd exchange_rates
-su -m exchange -c "python manage.py makemigrations exchange_rates"
-su -m exchange -c "python manage.py migrate"
-su -m exchange -c "python manage.py runserver 0.0.0.0:8000"
+echo "Generating migrations"
+su -m exchange -c "./manage.py makemigrations exchange_rates"
+su -m exchange -c "./manage.py makemigrations rates"
+echo "Applying migrations"
+su -m exchange -c "./manage.py migrate"
+su -m exchange -c "./manage.py runserver 0.0.0.0:8000"
